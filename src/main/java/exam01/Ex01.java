5ef559568f9f9411860dae5e6b670b033e868106
gmkil11 <mkkil4451@gmail.com>
2023-10-10 16:27:35 +0900

    maven proj

--- src/main/java/exam01/Ex01.java
+++ src/main/java/exam01/Ex01.java
@@ -2,14 +2,14 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Member member = new Member();
-        member.setUserId("user01");
-        member.setUserPw("123456");
-        member.setUserNm("user01");
+//        Member member = new Member();
+//        member.setUserId("user01");
+//        member.setUserPw("123456");
+//        member.setUserNm("user01");
+//
+//        System.out.println(member);
 
-        System.out.println(member);
-
-        Member member1 = new Member("user02", "123456", "user02");
-        System.out.println(member1);
+//        Member member1 = new Member("user02", "123456", "user02");
+//        System.out.println(member1);
     }
 }
--- src/main/java/exam01/Member.java
+++ src/main/java/exam01/Member.java
@@ -3,9 +3,19 @@ package exam01;
 
 import lombok.*;
 
-@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+//@Getter @Setter @ToString
+//@AllArgsConstructor // 매개변수로 추가하는 생성자
+//@NoArgsConstructor(access = AccessLevel.PRIVATE) // 액세스 레벨을 PRIVATE 로 설정 가능
+//@NoArgsConstructor // 기본 생성자
+//@EqualsAndHashCode // 두 가지 메서드가 재정의된다.
+@Data // @Getter, @Setter, @EqualsAndHashCode, @ToString
 public class Member {
     private String userId;
+
+//    @ToString.Exclude
+//    @NonNull
     private String userPw;
     private String userNm;
+
+
 }
