fac6a7ceb2ff83147a3929a2b8fe3465a74209bb
gmkil11 <mkkil4451@gmail.com>
2023-10-10 15:34:00 +0900

    maven proj

---
+++ src/main/java/exam01/Member.java
@@ -0,0 +1,12 @@
+package exam01;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+    private String userId;
+    private String userPw;
+    private String userNm;
+}
